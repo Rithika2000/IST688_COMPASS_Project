c38bc9ed3cd7e3b133b5ff1e7f0188a6c0589def
Rithika2000 <57192807+Rithika2000@users.noreply.github.com>
2024-11-17 23:40:14 +0000

    code

---
+++ sqlite-autoconf-3430100/chatbot.py
@@ -0,0 +1,10 @@
+import streamlit as st
+
+Compass_code = st.Page("COMPASS_code.py", title = 'Compass_code')
+
+
+pg = st.navigation([Compass_code])
+
+st.set_page_config(page_title = "HW Manager")
+
+pg.run()
